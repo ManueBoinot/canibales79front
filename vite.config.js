f9b06f5468009aa815e35d4043ed5e5e8d2beec7
antoine <antoine.giot@gmail.com>
2023-04-17 14:33:38 +0200

    mise à jour composer

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/js/app.js',
                 'resources/css/app.css',
+                'resources/sass/app.scss'
             ],
             refresh: true,
         }),
